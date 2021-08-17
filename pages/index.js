2431f8dad7b65c9cb5b8810d253bad8fa77542bd
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-08-17 23:27:30 +0700

    add link first post

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import React from "react";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 class Home extends React.Component {
   render() {
@@ -12,9 +13,12 @@ class Home extends React.Component {
         </Head>
         <section className={utilStyles.headingMd}>
           <p>[Your Self Introduction]</p>
+            <Link href="/posts/first-post">
+                <a className="bg-red-500">First Post</a>
+            </Link>
           <p>
             (This is a sample website - you’ll be building a site like this on{" "}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+            <a className="bg-blue-300" href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
         </section>
       </Layout>
