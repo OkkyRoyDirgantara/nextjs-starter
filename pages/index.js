8a0d2b5e8d0ced67b99b87f51cbbf6c8d2b8a4d2
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-08-17 13:31:07 +0700

    layouting

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import Head from "next/head";
 import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="title">
-        Read{" "}
-        <Link href="/posts/first-post">
-          <a>this page!</a>
-        </Link>
-      </h1>
-    </div>
-  );
+import React from "react";
+import Image from "next/image";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Image
+          src="/images/profile.jpg" // Route of the image file
+          height={144} // Desired size with correct aspect ratio
+          width={144} // Desired size with correct aspect ratio
+          alt="Your Name"
+        />
+        <h1 className="title">
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
+        </h1>
+      </div>
+    );
+  }
 }
+
+export default Home;
